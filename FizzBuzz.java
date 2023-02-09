ee01310fafd003b074b2151ac5b44a6bb0753074
ImThour <thouravi@gmail.com>
2023-02-09 14:22:59 +0530

    Update and rename FuzzBuzz.java to FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,7 @@
 // Write a program that outputs numbers from 1 to 100.
 // For multiples of 3, the program should output "Fizz" instead of the number, and for multiples of 5, it should output "Buzz".
 // For numbers that are multiples of both 3 and 5, it should output "FizzBuzz".
-public class FuzzBuzz {
+public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
